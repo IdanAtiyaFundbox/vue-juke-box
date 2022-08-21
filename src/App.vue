d3433bfe283f5e4d74332d390147f6811a758fc7
IdanAtiyaFundbox <idan.atiya@fundbox.com>
2022-08-22 02:14:03 +0300

    Added my project

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import HelloWorld from '@/components/HelloWorld.vue'
+import HomePage from './views/HomePage.vue';
+
+const k = 'idan'
+
+console.log('hsello')
+
+</script>
+
+<template>
+  <HomePage />
+  <HelloWorld />
+</template>
+
+<style scoped>
+</style>
---
+++ src/views/HomePage.vue
@@ -0,0 +1,17 @@
+<template>
+  <span>
+    Homepage
+  </span>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style lang="scss" scoped>
+
+span {
+  color: $red;
+}
+
+</style>
